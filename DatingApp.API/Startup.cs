f8dc22dd40f6400796e9876753f68ef012c26927
Yana_nk <karas.yana@gmail.com>
2020-10-07 00:18:40 +0300

    section publishing problem with sql creator

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -33,10 +33,23 @@ namespace DatingApp.API
 
         public IConfiguration Configuration { get; }
 
+         public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))); 
+            ConfigureServices(services);  
+        }
+
+         public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(x => x.UseMySql
+            (Configuration.GetConnectionString("DefaultConnection"))); 
+            ConfigureServices(services);  
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+            
             services.AddControllers().AddNewtonsoftJson(opt =>
             {
                 opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
@@ -58,6 +71,8 @@ namespace DatingApp.API
             });
             services.AddScoped<LogUserActivity>();
         }
+
+       
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -88,11 +103,12 @@ namespace DatingApp.API
             app.UseAuthorization();
             app.UseCors(x =>x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             
-
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                //endpoints.MapFallbackToController("Index", "Fallback");
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
