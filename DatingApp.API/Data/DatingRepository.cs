68f41057bff7b8b2147bd68a961086e22a5ea806
Yana_nk <karas.yana@gmail.com>
2020-10-07 14:48:18 +0300

    pre publishing

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -39,13 +39,13 @@ namespace DatingApp.API.Data
 
         public async Task<User> GetUser(int id)
         {
-           var user = await _context.Users.Include(p =>p.Photos).FirstOrDefaultAsync(u => u.Id == id);
+           var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == id);
            return user;
         }
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users =  _context.Users.Include(p => p.Photos)
+            var users =  _context.Users
                 .OrderByDescending(u => u.LastActive).AsQueryable();
             users = users.Where(u => u.Id != userParams.UserId);
             users = users.Where(u => u.Gender == userParams.Gender);
@@ -88,8 +88,6 @@ namespace DatingApp.API.Data
         private async Task<IEnumerable<int>> GetUserLikes (int id,bool likers)
         {
             var user = await _context.Users
-            .Include(x => x.Likers)
-            .Include(x => x.Likees)
             .FirstOrDefaultAsync(u => u.Id ==id);
 
             if (likers)
@@ -117,8 +115,6 @@ namespace DatingApp.API.Data
         public async Task<PagedList<Message>> GetMessagesForUser(MessageParams messageParams)
         {
             var messages =  _context.Messages
-            .Include(u => u.Sender).ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient).ThenInclude( p => p.Photos)
             .AsQueryable();
 
             switch(messageParams.MessageContainer)
@@ -145,8 +141,6 @@ namespace DatingApp.API.Data
         public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
         {
            var messages =  await _context.Messages
-            .Include(u => u.Sender).ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient).ThenInclude( p => p.Photos)
             .Where(u => u.RecipientId == userId && u.RecipientDeleted == false && u.SenderId == recipientId 
             || u.RecipientId == recipientId && u.SenderId == userId && u.SenderDeleted ==false)
             .OrderByDescending( u => u.MessageSent)
--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -10,7 +10,7 @@ namespace DatingApp.API.Models
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
         public string PublicId { get; set; }
-        public User user { get; set; }
+        public virtual User user { get; set; }
         public int UserId { get; set; }
     }
 }
